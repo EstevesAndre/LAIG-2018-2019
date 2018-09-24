746755faeda5deffcd89f5d64659054f17fd9511
estevesandre <up201606673@fe.up.pt>
2018-09-25 00:26:15 +0100

    deleted junk

--- TP1/MySceneGraph.js
+++ TP1/MySceneGraph.js
@@ -778,7 +778,9 @@ class MySceneGraph {
      * @param {textures block element} texturesNode
      */
     parseTextures(texturesNode) {
-        // TODO: Parse block
+        
+        var textures = [];
+        
 
         console.log("Parsed textures");
 
@@ -832,133 +834,6 @@ class MySceneGraph {
 
         return null;
     }
-
-    // /**
-    //  * Parses the <INITIALS> block.
-    //  */
-    // parseInitials(initialsNode) {
-
-    //     var children = initialsNode.children;
-
-    //     var nodeNames = [];
-
-    //     for (var i = 0; i < children.length; i++)
-    //         nodeNames.push(children[i].nodeName);
-
-    //     // Frustum planes
-    //     // (default values)
-    //     this.near = 0.1;
-    //     this.far = 500;
-    //     var indexFrustum = nodeNames.indexOf("frustum");
-    //     if (indexFrustum == -1) {
-    //         this.onXMLMinorError("frustum planes missing; assuming 'near = 0.1' and 'far = 500'");
-    //     }
-    //     else {
-    //         this.near = this.reader.getFloat(children[indexFrustum], 'near');
-    //         this.far = this.reader.getFloat(children[indexFrustum], 'far');
-
-    //         if (!(this.near != null && !isNaN(this.near))) {
-    //             this.near = 0.1;
-    //             this.onXMLMinorError("unable to parse value for near plane; assuming 'near = 0.1'");
-    //         }
-    //         else if (!(this.far != null && !isNaN(this.far))) {
-    //             this.far = 500;
-    //             this.onXMLMinorError("unable to parse value for far plane; assuming 'far = 500'");
-    //         }
-
-    //         if (this.near >= this.far)
-    //             return "'near' must be smaller than 'far'";
-    //     }
-
-    //     // Checks if at most one translation, three rotations, and one scaling are defined.
-    //     if (initialsNode.getElementsByTagName('translation').length > 1)
-    //         return "no more than one initial translation may be defined";
-
-    //     if (initialsNode.getElementsByTagName('rotation').length > 3)
-    //         return "no more than three initial rotations may be defined";
-
-    //     if (initialsNode.getElementsByTagName('scale').length > 1)
-    //         return "no more than one scaling may be defined";
-
-    //     // Initial transforms.
-    //     this.initialTranslate = [];
-    //     this.initialScaling = [];
-    //     this.initialRotations = [];
-
-    //     // Gets indices of each element.
-    //     var translationIndex = nodeNames.indexOf("translation");
-    //     var thirdRotationIndex = nodeNames.indexOf("rotation");
-    //     var secondRotationIndex = nodeNames.indexOf("rotation", thirdRotationIndex + 1);
-    //     var firstRotationIndex = nodeNames.lastIndexOf("rotation");
-    //     var scalingIndex = nodeNames.indexOf("scale");
-
-    //     // Checks if the indices are valid and in the expected order.
-    //     // Translation.
-    //     this.initialTransforms = mat4.create();
-    //     mat4.identity(this.initialTransforms);
-
-    //     if (translationIndex == -1)
-    //         this.onXMLMinorError("initial translation undefined; assuming T = (0, 0, 0)");
-    //     else {
-    //         var tx = this.reader.getFloat(children[translationIndex], 'x');
-    //         var ty = this.reader.getFloat(children[translationIndex], 'y');
-    //         var tz = this.reader.getFloat(children[translationIndex], 'z');
-
-    //         if (tx == null || ty == null || tz == null) {
-    //             tx = 0;
-    //             ty = 0;
-    //             tz = 0;
-    //             this.onXMLMinorError("failed to parse coordinates of initial translation; assuming zero");
-    //         }
-
-    //         //TODO: Save translation data
-    //     }
-
-    //     //TODO: Parse Rotations
-
-    //     //TODO: Parse Scaling
-
-    //     //TODO: Parse Reference length
-
-    //     this.log("Parsed initials");
-
-    //     return null;
-    // }
-
-
-    // /**
-    //  * Parses the <TEXTURES> block. 
-    //  * @param {textures block element} texturesNode
-    //  */
-    // parseTextures(texturesNode) {
-    //     // TODO: Parse block
-
-    //     console.log("Parsed textures");
-
-    //     return null;
-    // }
-
-    // /**
-    //  * Parses the <MATERIALS> node.
-    //  * @param {materials block element} materialsNode
-    //  */
-    // parseMaterials(materialsNode) {
-    //     // TODO: Parse block
-    //     this.log("Parsed materials");
-    //     return null;
-
-    // }
-
-    // /**
-    //  * Parses the <NODES> block.
-    //  * @param {nodes block element} nodesNode
-    //  */
-    // parseNodes(nodesNode) {
-    //     // TODO: Parse block
-    //     this.log("Parsed nodes");
-    //     return null;
-    // }
-
 
     /*
      * Callback to be executed on any read error, showing an error on the console.
