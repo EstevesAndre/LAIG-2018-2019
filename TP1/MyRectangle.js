5c4696ca8acfe4fc730497aa170e98dfc12afe44
luisdiogo98 <luisdiogo98@users.noreply.github.com>
2018-10-03 12:16:05 +0000

    minor fix

--- TP1/MyRectangle.js
+++ TP1/MyRectangle.js
@@ -28,8 +28,8 @@ class MyRectangle extends CGFobject
 			];
 
 		this.indices = [
-                0, 1, 3,
-                1, 2, 3
+                0, 3, 1,
+                1, 3, 2
 			];
 
 		this.normals = [
