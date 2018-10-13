4d8eda405c6375090b86588c2d4078dde2dc94af
estevesandre <up201606673@fe.up.pt>
2018-10-13 21:13:34 +0100

    restructured  Cylinder primitive

--- TP1/primitives/Cylinder.js
+++ TP1/primitives/Cylinder.js
@@ -16,7 +16,6 @@ class Cylinder extends CGFobject
         this.slices = slices;
         this.stacks = stacks;
 
-        this.face = new Circle(scene, this.slices);
 
 		this.initBuffers();
 	};
@@ -24,15 +23,13 @@ class Cylinder extends CGFobject
 	initBuffers() 
 	{
 		this.vertices = [];
-
         this.indices = [];
-
         this.normals = [];
-
         this.texCoords = [];
 
         var angle = (2* Math.PI) / this.slices;
-        var division = 1.0 / this.stacks;
+		var division = this.height / this.stacks;		
+		var radiusStack = (this.top - this.base) / this.stacks;
 
         // Here we do 1 division per iteration so it's k <= this.stacks.
 		// We actually do k + 1 divisions to have exactly the right number of stacks
@@ -40,8 +37,13 @@ class Cylinder extends CGFobject
 		{
 			for(let i = 0; i <= this.slices; i++)
 			{
-				this.vertices.push(Math.cos(i * angle), 
-								   Math.sin(i * angle),
+				console.log(
+					Math.cos(i * angle) * (this.base + radiusStack * k), 
+					Math.sin(i * angle) * (this.base + radiusStack * k),
+					k * division
+				);
+				this.vertices.push(Math.cos(i * angle) * (this.base + radiusStack * k), 
+								   Math.sin(i * angle) * (this.base + radiusStack * k),
 								   k * division);
 				 
 				this.normals.push(Math.cos(i * angle),
@@ -49,7 +51,7 @@ class Cylinder extends CGFobject
 								  0);
 			
 				// Place to texCoords				
-				this.texCoords.push(1-i/this.slices,k*division);
+				this.texCoords.push(1-i/this.slices, k * division / this.height);
 				
 				if(k != 0 && i != 0)
 				{
@@ -62,30 +64,47 @@ class Cylinder extends CGFobject
 									  (this.slices+1)*k + i);
 				}
 			}
-        }
+		}
+		
+		// Center
+		this.vertices.push(0,0,0);
+		this.normals.push(0,0,-1);
+		this.texCoords.push(0.5,0.5);		
+		let indiceRef = this.vertices.length / 3 -  1;
+		
+		for(let k = 1; k <= 2; k++)
+		{
+			if(k == 2) indiceRef += this.slices + 2;
+
+			for(let i = 0; i <= this.slices; i++)
+			{
+				this.vertices.push(	Math.cos(i* angle) * (k == 1 ? this.base : this.top),
+									Math.sin(i * angle) * (k == 1 ? this.base : this.top),
+									(k == 1 ? 0 : this.height));
+				
+				k == 1 ? this.normals.push(0,0,-1) : this.normals.push(0,0,1);
+				
+				this.texCoords.push(0.5 + Math.cos(i * angle) / 2, 0.5 - Math.sin(i * angle) / 2);
+
+				if(i != 0)
+				{
+					if(i == this.slices-1)
+					{
+						k == 1 ? this.indices.push(indiceRef,indiceRef + 1,indiceRef + i+1)
+								: this.indices.push(indiceRef,indiceRef + i+1,indiceRef + 1);
+					}
+					
+					k == 1 ? this.indices.push(indiceRef,indiceRef + i+1,indiceRef + i)
+							: this.indices.push(indiceRef,indiceRef + i,indiceRef + i+1);
+				}
+			}				
+			this.vertices.push(0,0,this.height);
+			this.normals.push(0,0,1);
+			this.texCoords.push(0.5,0.5);
+		}
         
 		this.primitiveType=this.scene.gl.TRIANGLES;
 		this.initGLBuffers();
     };
-    
-    display()
-    {
-		this.scene.pushMatrix();
-			this.scene.scale(this.base,this.top,this.height);
-			this.drawElements(this.scene.gl.TRIANGLES);
-		this.scene.popMatrix();
-		
-        this.scene.pushMatrix();
-        	this.scene.scale(this.top, this.top, 1);
-            this.scene.translate(0, 0, this.height);
-            this.face.display();
-        this.scene.popMatrix();
-        
-        this.scene.pushMatrix();            
-            this.scene.scale(this.base, this.base, 1);
-            this.scene.rotate(Math.PI, 1, 0, 0);
-            this.face.display();
-		this.scene.popMatrix();
-		
-    };
+
 };
