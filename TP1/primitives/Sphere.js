8286a71d0c7be12435dca595a1443d7c2b9dd797
estevesandre <up201606673@fe.up.pt>
2018-10-14 01:02:41 +0100

    sphere done, needed to test textures

--- TP1/primitives/Sphere.js
+++ TP1/primitives/Sphere.js
@@ -20,121 +20,49 @@ class Sphere extends CGFobject
 	initBuffers() 
 	{
 		this.vertices = [];
-
 		this.indices = [];
-
 		this.normals = [];
-
 		this.texCoords = [];
 
-		var angle = (2* Math.PI) / this.slices;
-		var division = 1.0 / this.stacks;
-		
-		let alpha_angle = Math.PI/(2*this.stacks);
-		for(let k = 0; k <= this.stacks; k++)
-		{
-			// last stack
-			if(k == this.stacks)
-			{
-				this.vertices.push(	this.radius * Math.cos((this.slices-1) * angle)*Math.cos(Math.asin(1)), 
-									this.radius * Math.sin((this.slices-1) * angle)*Math.cos(Math.asin(1)), 
-									this.radius * 1);
-				this.normals.push(	Math.cos((this.slices-1) * angle), 
-									Math.sin((this.slices-1) * angle), 
-									Math.cos(Math.asin(1)) );
-				this.texCoords.push(0.5,0.5);
-					
-				for(let i = 0; i <= this.slices; i++)
-				{
-					this.indices.push((this.slices+1)*k,(this.slices+1)*(k-1) + i, (this.slices+1)*(k-1) + i + 1);				
-				}	
-			}
-			else
-			{
-				for(let i = 0; i <= this.slices; i++)
-				{
-					this.vertices.push(	this.radius * Math.cos(i * angle)*Math.cos(Math.asin(division*(k))), 
-										this.radius * Math.sin(i * angle)*Math.cos(Math.asin(division*(k))), 
-										this.radius * k * division);
-					this.normals.push(	Math.cos(i * angle), 
-										Math.sin(i * angle), 
-										Math.cos(Math.asin(division*(k))) ) ;
-									
-					this.texCoords.push( (Math.cos(i * angle)*Math.cos(Math.asin(division*(k))) ) / 2.0 + 0.5, 
-										-(Math.sin(i * angle)*Math.cos(Math.asin(division*(k))) ) / 2.0 + 0.5);				
-										
-								/*	console.log( "STACK = " + k + " SLICE = " + i + ", " +
-											((Math.cos(i * angle)*Math.cos(Math.asin(division*(k))) ) / 2.0 + 0.5) 
-											+ " || " +
-											(-(Math.sin(i * angle)*Math.cos(Math.asin(division*(k))) ) / 2.0 + 0.5));*/
-					
-					if(k != 0 && i != 0)
-					{
-						this.indices.push(	(this.slices+1)*k + i - 1, 
-											(this.slices+1)*(k-1) + i - 1, 
-											(this.slices+1)*(k-1) + i);
+		var angleSlices = (2*Math.PI)/this.slices;
+		var angleStacks = Math.PI/this.stacks;
+		var inc = 0;
+		var mult = 1;
 
-						this.indices.push(	(this.slices+1)*k + i - 1, 
-											(this.slices+1)*(k-1) + i , 
-											(this.slices+1)*k + i);
-						
-					}
-				}
-			}
-		}
-		var numVertices = this.vertices.length / 3;
 		for(let k = 0; k <= this.stacks; k++)
 		{
-			// last stack
-			if(k == this.stacks)
-			{
-				this.vertices.push(	this.radius * Math.cos((this.slices-1) * angle)*Math.cos(Math.asin(1)), 
-									this.radius * Math.sin((this.slices-1) * angle)*Math.cos(Math.asin(1)), 
-									this.radius * -1);
-				this.normals.push(	Math.cos((this.slices-1) * angle), 
-									Math.sin((this.slices-1) * angle), 
-									-Math.cos(Math.asin(1)) );
-				this.texCoords.push(0.5,0.5);
-					
-				for(let i = 0; i <= this.slices; i++)
-				{
-					this.indices.push(	numVertices + (this.slices+1)*k,
-										numVertices + (this.slices+1)*(k-1) + i + 1,
-										numVertices + (this.slices+1)*(k-1) + i
-									);				
-				}	
-			}
-			else
-			{
-				for(let i = 0; i <= this.slices; i++)
-				{
-					this.vertices.push(	this.radius * Math.cos(i * angle)*Math.cos(Math.asin(division*(k))), 
-										this.radius * Math.sin(i * angle)*Math.cos(Math.asin(division*(k))), 
-										this.radius * -k * division);
-					this.normals.push(	Math.cos(i * angle), 
-										Math.sin(i * angle), 
-										-Math.cos(Math.asin(division*(k))) ) ;
-									
-					this.texCoords.push( (Math.cos(i * angle)*Math.cos(Math.asin(division*(k))) ) / 2.0 + 0.5, 
-										-(Math.sin(i * angle)*Math.cos(Math.asin(division*(k))) ) / 2.0 + 0.5);					
+			for(let i = 0; i <= this.slices; i++)
+			{	
+				this.vertices.push(	Math.cos(i * angleSlices) * (this.radius * Math.cos(k * angleStacks)),
+									Math.sin(i * angleSlices) * (this.radius * Math.cos(k * angleStacks)),
+									mult * this.radius * Math.sin(k * angleStacks));				
 				
-					if(k != 0 && i != 0)
-					{
-						this.indices.push(	numVertices + (this.slices+1)*k + i - 1, 
-											numVertices + (this.slices+1)*(k-1) + i,											
-											numVertices + (this.slices+1)*(k-1) + i - 1
-											);
+				this.normals.push( 	Math.cos(i * angleSlices),
+									Math.sin(i * angleSlices),
+									Math.sin(k * angleStacks));
+				
+				mult == 1 ? this.texCoords.push(Math.cos(i * angleSlices), Math.sin(k * angleStacks) / 2.0 + 0.5) : 
+							this.texCoords.push(Math.cos(i * angleSlices), -Math.sin(k * angleStacks) / 2.0 + 0.5);
 
-						this.indices.push(	numVertices + (this.slices+1)*k + i - 1, 
-											numVertices + (this.slices+1)*k + i,
-											numVertices + (this.slices+1)*(k-1) + i
-											);
-						
-					}
+				if(k != 0 && i != 0)
+				{					
+					this.indices.push(	(this.slices+1)*k + i - 1 + inc, 
+										(this.slices+1)*(k-1) + i - 1 + inc, 
+										(this.slices+1)*(k-1) + i + inc);
+
+					this.indices.push(	(this.slices+1)*k + i - 1 + inc, 
+										(this.slices+1)*(k-1) + i + inc, 
+										(this.slices+1)*k + i + inc);					
 				}
 			}
+
+			if(k == this.stacks / 2.0 && mult == 1)
+			{
+				inc = this.slices + 1;					
+				mult = -1;
+				k--;
+			}
 		}
-		
 
 		this.primitiveType=this.scene.gl.TRIANGLES;
 		this.initGLBuffers();
