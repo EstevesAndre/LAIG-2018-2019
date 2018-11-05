906c712c0734d6eaca31f6435942a4bbb2d4140d
EstevesAndre <up201606673@fe.up.pt>
2018-11-05 22:32:36 +0000

    updated animation

--- TP2/animations/Animation.js
+++ TP2/animations/Animation.js
@@ -8,16 +8,6 @@ class Animation
         this.angRotation = 0.0;
     };
 
-    getTime() // miliseconds
-    {
-        return this.timeElapsed;
-    };
-
-    decreaseTime(delta) // miliseconds
-    {
-        this.timeElapsed += delta;
-    }
-
     isAnimationOver()
     {
         return this.timeElapsed > this.time;
