f0591ec9686210a7b90d8ff16bea25e7dd565984
EstevesAndre <up201606673@fe.up.pt>
2018-11-08 16:02:08 +0000

    final update on Plane

--- TP2/primitives/Plane.js
+++ TP2/primitives/Plane.js
@@ -7,34 +7,25 @@ class Plane extends CGFobject {
         this.npartsU = npartsU;
         this.npartsV = npartsV;
         
-        this.plane = null;
-
-        this.makeSurface(
-            1, // degree on U: degreeU + 1 control vertexes U
-            1, // degree on V: degreeV + 1 control vertexes on V
+        var nurbsSurface = new CGFnurbsSurface(
+            1, 
+            1, 
             [	// U = 0
-               [ // V = 0..1;
-                    [0.5, 0.0, -0.5, 1 ],
-                    [0.5, 0.0,  0.5, 1 ]								
-               ],
-               // U = 1
-               [ // V = 0..1
-                   [-0.5, 0.0, -0.5, 1 ],
-                   [-0.5, 0.0, 0.5, 1 ]				 
-               ]
-           ]
+                [ // V = 0..1;
+                     [0.5, 0.0, -0.5, 1 ],
+                     [0.5, 0.0,  0.5, 1 ]								
+                ],
+                // U = 1
+                [ // V = 0..1
+                    [-0.5, 0.0, -0.5, 1 ],
+                    [-0.5, 0.0, 0.5, 1 ]				 
+                ]
+            ]
         );
+
+        this.plane = new CGFnurbsObject(this.scene, this.npartsU, this.npartsV, nurbsSurface); // must provide an object with the function getPoint(u, v) (CGFnurbsSurface has it);
     };
 
-    makeSurface(degree1, degree2, controlvertexes) {
-		
-		var nurbsSurface = new CGFnurbsSurface(degree1, degree2, controlvertexes);
-
-        var obj = new CGFnurbsObject(this.scene, this.npartsU, this.npartsV, nurbsSurface ); // must provide an object with the function getPoint(u, v) (CGFnurbsSurface has it)
-        
-		this.plane = obj;
-    };
-    
     display()
     {
         this.plane.display();        
