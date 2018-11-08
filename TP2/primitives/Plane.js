c111c88b5ffa9ea2058414fab6e12dbd3c9a39b2
EstevesAndre <up201606673@fe.up.pt>
2018-11-08 15:16:54 +0000

    fixed plane constructor

--- TP2/primitives/Plane.js
+++ TP2/primitives/Plane.js
@@ -6,50 +6,38 @@ class Plane extends CGFobject {
 
         this.npartsU = npartsU;
         this.npartsV = npartsV;
-
+        
         this.plane = null;
-        this.createSurface();
+
+        this.makeSurface(
+            1, // degree on U: degreeU + 1 control vertexes U
+            1, // degree on V: degreeV + 1 control vertexes on V
+            [	// U = 0
+               [ // V = 0..1;
+                    [0.5, 0.0, -0.5, 1 ],
+                    [0.5, 0.0,  0.5, 1 ]								
+               ],
+               // U = 1
+               [ // V = 0..1
+                   [-0.5, 0.0, -0.5, 1 ],
+                   [-0.5, 0.0, 0.5, 1 ]				 
+               ]
+           ]
+        );
     };
 
-    createSurface()
-    {
-        var controlvertexes = [];
-        var controlPointsV = [];
-        let xdiv = this.npartsV / 2;
-        let zdiv = this.npartsU / 2;
-
-        for(let j = 0; j <= this.npartsU; j++)
-        {
-            controlPointsV = [];
-            for(let i = 0; i <= this.npartsV; i++)
-            {
-                controlPointsV.push([xdiv - j, 0, -zdiv + i, 1 ]);
-            }
-            controlvertexes.push(controlPointsV);
-        }
-
-        console.log(controlvertexes);
-
-        this.makeSurface(this.npartsU, // degree on U: npartsU + 1 control vertexes U
-						 this.npartsV, // degree on V: npartsV + 1 control vertexes on V
-						controlvertexes
-                        );
-
-    };
-    
     makeSurface(degree1, degree2, controlvertexes) {
 		
 		var nurbsSurface = new CGFnurbsSurface(degree1, degree2, controlvertexes);
 
-        var obj = new CGFnurbsObject(this.scene, 20, 20, nurbsSurface ); // must provide an object with the function getPoint(u, v) (CGFnurbsSurface has it)
+        var obj = new CGFnurbsObject(this.scene, this.npartsU, this.npartsV, nurbsSurface ); // must provide an object with the function getPoint(u, v) (CGFnurbsSurface has it)
         
 		this.plane = obj;
     };
     
     display()
     {
-        if(this.plane != null)
-            this.plane.display();        
+        this.plane.display();        
     };
    
 };
