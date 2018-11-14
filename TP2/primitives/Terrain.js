6ae50aa08cf8226c9294f420d95dea4a7ce983f5
luisdiogo98 <luisdiogo98@users.noreply.github.com>
2018-11-14 09:24:49 +0000

    Finished terrain

--- TP2/primitives/Terrain.js
+++ TP2/primitives/Terrain.js
@@ -12,11 +12,12 @@ class Terrain extends CGFobject {
         this.shader = new CGFshader(this.scene.gl, "shaders/vertexShader.vert", "shaders/fragShader.frag");
         this.shader.setUniformsValues({text: 1});
         this.shader.setUniformsValues({height: 2});
+        this.shader.setUniformsValues({scale: heightscale});
         
         this.texture = new CGFtexture(this.scene, this.idtexture);
         this.height = new CGFtexture(this.scene, this.idheightmap);
-
-        this.wing_outter = new Plane(this.scene, 50,50);
+        console.log(parts);
+        this.plane = new Plane(this.scene, parts,parts);
     };
 
     display()
@@ -24,8 +25,7 @@ class Terrain extends CGFobject {
         this.scene.setActiveShader(this.shader);
         this.texture.bind(1);
         this.height.bind(2);
-        this.scene.scale(20,20,20,1);
-        this.wing_outter.display();
+        this.plane.display();
         this.scene.setActiveShader(this.scene.defaultShader);
     };
 };
--- TP2/shaders/vertexShader.vert
+++ TP2/shaders/vertexShader.vert
@@ -6,11 +6,12 @@ attribute vec2 aTextureCoord;
 varying vec2 vTextureCoord;
 
 uniform sampler2D height;
+uniform float scale;
 
 void main()
 {
     vTextureCoord = aTextureCoord;
 
-    vec3 newPos = vec3(aVertexPosition.x, aVertexPosition.y + texture2D(height, aTextureCoord)[1] * 0.2, aVertexPosition.z);
+    vec3 newPos = vec3(aVertexPosition.x, aVertexPosition.y + texture2D(height, aTextureCoord)[1] * scale, aVertexPosition.z);
     gl_Position = uPMatrix * uMVMatrix * vec4(newPos,1.0);
 }
