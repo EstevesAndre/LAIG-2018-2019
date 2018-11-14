3dd751677bd7cb90fbd3543a9058800e25df2b3a
EstevesAndre <up201606673@fe.up.pt>
2018-11-14 01:36:19 +0000

    updated vehicle

--- TP2/primitives/Vehicle.js
+++ TP2/primitives/Vehicle.js
@@ -52,7 +52,7 @@ class Vehicle extends CGFobject
         );
         
         this.body = this.makeSurface
-            (6,4, 2,2,
+            (12,8, 2,2,
             [
                 [
                     [-0.5, 0.0,  1.0, 1.0],
@@ -73,7 +73,7 @@ class Vehicle extends CGFobject
         );
 
         this.bodySideRight = this.makeSurface
-            (6,4, 2,2,
+            (8,8, 2,2,
             [
                 [
                     [0.5, 0.0,   1.0, 1.0],
@@ -94,7 +94,7 @@ class Vehicle extends CGFobject
         );
 
         this.bodySideLeft = this.makeSurface
-            (6,4, 2,2,
+            (8,8, 2,2,
             [
                 [
                     [-0.5, 0.4,  -2.0, 1.0],
@@ -115,7 +115,7 @@ class Vehicle extends CGFobject
         );
 
         this.bodyDown = this.makeSurface
-            (6,4, 2,2,
+            (12,8, 2,2,
             [
                 [
                     [-0.5, 0.4, -2.0, 1.0],
@@ -135,28 +135,29 @@ class Vehicle extends CGFobject
             ]
         );
 
-        this.window = this.makeSurface
-            (6,4, 2,2,
-            [
-                [
-                    [-0.5, 0.0,  1.0, 1.0],
-                    [-0.5, 0.6, 0.25, 1.0],
-                    [-0.5, 0.8, -0.5, 1.0]
-                ],
-                [
-                    [0.0, 0.2,  1.5, 1.0],
-                    [0.0, 1.0,  0.5, 1.0],
-                    [0.0, 1.0, -1.0, 1.0]
-                ],
-                [
-                    [0.5, 0.0,  1.0, 1.0],
-                    [0.5, 0.6, 0.25, 1.0],
-                    [0.5, 0.8, -0.5, 1.0]
-                ]
-            ]
-        );
 
-        this.side_connector = new Cylinder2(this.scene, 0.2, 0.4, 0.7, 32, 3);
+        this.supportBars = new Cylinder2(this.scene, 0.04, 0.04, 1.0, 8, 2);
+        this.sphere = new Sphere(this.scene, 0.04, 12, 8);
+        this.side_connector = new Cylinder2(this.scene, 0.2, 0.4, 0.7, 8, 3);
+        
+        this.blackTexture = new CGFappearance(this.scene);
+        this.blackTexture.loadTexture("scenes/images/black.jpg");
+        this.blackTexture.setAmbient(0.75,0.75,0.75,1);
+
+        this.metalTexture = new CGFappearance(this.scene);
+        this.metalTexture.loadTexture("scenes/images/propeller.png");
+        this.metalTexture.setAmbient(0.25,0.25,0.25,1);
+        this.metalTexture.setSpecular(0.7,0.7,0.7,1);
+
+        this.bodyTexture = new CGFappearance(this.scene);
+        this.bodyTexture.loadTexture("scenes/images/vehiclebody.jpg");
+        this.bodyTexture.setAmbient(0.5,0.5,0.5,1);
+        this.bodyTexture.setSpecular(0.7,0.7,0.7,1);
+        
+        this.windowTexture = new CGFappearance(this.scene);
+        this.windowTexture.loadTexture("scenes/images/window.jpg");
+        this.windowTexture.setAmbient(0.25,0.25,0.25,1);
+        this.windowTexture.setSpecular(0.7,0.7,0.7,1);
     };
 
     makeSurface(npartsU, npartsV, degree1, degree2, controlvertexes) {
@@ -178,6 +179,7 @@ class Vehicle extends CGFobject
             this.scene.translate(1.5,0,1.5);
             this.scene.rotate(-Math.PI/2.0, 0,1,0);
             this.scene.scale(1,0.15,1);
+            this.blackTexture.apply();
             this.side_connector.display();
         this.scene.popMatrix();
         
@@ -190,6 +192,7 @@ class Vehicle extends CGFobject
             this.scene.translate(-1.5,0,1.5);
             this.scene.rotate(Math.PI/2.0, 0,1,0);
             this.scene.scale(1,0.15,1);
+            this.blackTexture.apply();
             this.side_connector.display();
         this.scene.popMatrix();
 
@@ -204,6 +207,7 @@ class Vehicle extends CGFobject
             this.scene.translate(1.7,0.7,-2.5);
             this.scene.rotate(-Math.PI/2.0, 0,1,0);
             this.scene.scale(1.3,0.195,1.3);
+            this.blackTexture.apply();
             this.side_connector.display();
         this.scene.popMatrix();
         
@@ -217,19 +221,23 @@ class Vehicle extends CGFobject
             this.scene.translate(-1.7,0.7,-2.5);
             this.scene.rotate(Math.PI/2.0, 0,1,0);
             this.scene.scale(1.3,0.195,1.3);
+            this.blackTexture.apply();
             this.side_connector.display();
         this.scene.popMatrix();
         
         this.scene.pushMatrix();
             this.scene.scale(2.0,2.0,2.0);
+            this.windowTexture.apply();
             this.body.display();
+            this.bodyTexture.apply();
             this.bodyDown.display();
             this.bodySideRight.display();
             this.bodySideLeft.display();
         this.scene.popMatrix();
         
         this.scene.pushMatrix();
-
+            this.blackTexture.apply();
+            this.displaySupportBars();
         this.scene.popMatrix();
         
         // Need to be changed
@@ -237,10 +245,61 @@ class Vehicle extends CGFobject
         this.wing_angRot %= 2*Math.PI;
     };
 
+    displaySupportBars()
+    {
+        this.scene.pushMatrix();
+            this.scene.translate(0.6, -0.9, 0.4);
+            this.scene.rotate(Math.PI/2.0, 1,0,0);
+            this.scene.scale(1,1,0.6);
+            this.supportBars.display();
+        this.scene.popMatrix();
+        
+        this.scene.pushMatrix();
+            this.scene.translate(-0.6, -0.9, 0.4);
+            this.scene.rotate(Math.PI/2.0, 1,0,0);
+            this.scene.scale(1,1,0.6);
+            this.supportBars.display();
+        this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+            this.scene.translate(0.6, -0.4, -2);
+            this.scene.rotate(Math.PI/2.0, 1,0,0);
+            this.scene.scale(1,1,1.1);
+            this.supportBars.display();
+        this.scene.popMatrix();
+        
+        this.scene.pushMatrix();
+            this.scene.translate(-0.6, -0.4, -2);
+            this.scene.rotate(Math.PI/2.0, 1,0,0);
+            this.scene.scale(1,1,1.1);
+            this.supportBars.display();
+        this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+            this.scene.translate(-0.6, -1.5, -2.5);
+            this.scene.scale(1,1,3.5);
+            this.supportBars.display();
+            this.sphere.display();
+            this.scene.translate(0.0, 0.0, 1.0);
+            this.sphere.display();
+        this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+            this.scene.translate(0.6, -1.5, -2.5);
+            this.scene.scale(1,1,3.5);
+            this.supportBars.display();
+            this.sphere.display();
+            this.scene.translate(0.0, 0.0, 1.0);
+            this.sphere.display();
+        this.scene.popMatrix();
+    }
+
     displayWing()
     {
         this.scene.pushMatrix();        
             this.scene.scale(0.75,0.75,0.75);
+            
+            this.metalTexture.apply();
 
             this.scene.pushMatrix();
                 this.scene.pushMatrix();
@@ -251,6 +310,8 @@ class Vehicle extends CGFobject
 
                 this.scene.translate(0,-0.09,0);            
                 this.scene.scale(1,0.5,1);
+
+                this.blackTexture.apply();
 
                 this.scene.rotate(Math.PI/2.0, 0,1,0);
                 this.wing_part.display();
@@ -271,6 +332,8 @@ class Vehicle extends CGFobject
             this.scene.pushMatrix();
                 this.scene.translate(0,0.035,0.0);
                 
+                this.metalTexture.apply();
+
                 this.scene.rotate(Math.PI/3.0 + this.wing_angRot, 0,1,0);
                 this.wing_propeller.display();
 
