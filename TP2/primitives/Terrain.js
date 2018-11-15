5895471ecc5fd00f126e26f51853e2f3e691f0b6
EstevesAndre <up201606673@fe.up.pt>
2018-11-15 00:40:23 +0000

    tested scene

--- TP2/primitives/Terrain.js
+++ TP2/primitives/Terrain.js
@@ -3,20 +3,14 @@ class Terrain extends CGFobject {
     constructor(scene, idtexture, idheightmap, parts, heightscale)
     {
         super(scene);
-
-        this.idtexture = idtexture;
-        this.idheightmap = idheightmap;
-        this.parts = parts;
-        this.heightscale = heightscale;
         
         this.shader = new CGFshader(this.scene.gl, "shaders/vertexShader.vert", "shaders/fragShader.frag");
         this.shader.setUniformsValues({text: 1});
         this.shader.setUniformsValues({height: 2});
         this.shader.setUniformsValues({scale: heightscale});
         
-        this.texture = new CGFtexture(this.scene, this.idtexture);
-        this.height = new CGFtexture(this.scene, this.idheightmap);
-        console.log(parts);
+        this.texture = new CGFtexture(this.scene, idtexture);
+        this.height = new CGFtexture(this.scene, idheightmap);
         this.plane = new Plane(this.scene, parts,parts);
     };
 
